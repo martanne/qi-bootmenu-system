caf662dd5785d0811942ce1111b4205941bb3666
Marc Andre Tanner <mat@brain-dump.org>
2009-10-09 23:50:00 +0200

    dialog: use $CFLAGS and add support for static compilation
    Signed-off-by: Marc Andre Tanner <mat@brain-dump.org>

--- sources/sections/dialog-elementary.sh
+++ sources/sections/dialog-elementary.sh
@@ -1,8 +1,12 @@
 setupfor dialog-elementary 
 
-$CC `pkg-config --cflags --libs elementary` -lz -ljpeg -lfreetype -lts \
-	dialog.c -o dialog || dienow 
+$CC $CFLAGS $CROSS_CFLAGS `pkg-config --cflags --libs elementary` \
+	-lz -ljpeg -lfreetype -lts dialog.c -o dialog || dienow 
+
+#$CC $CFLAGS $CROSS_CFLAGS `pkg-config --cflags --libs elementary --static` \
+#	-lz -ljpeg -lfreetype -lts -static dialog.c -o dialog-static || dienow 
 
 cp dialog $ROOT_DIR/usr/bin || dienow 
+#cp dialog-static $ROOT_DIR/usr/bin || dienow 
 
 cleanup dialog-elementary 
