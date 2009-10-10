67cde270109db30f927b921c180b5b63b88a4aa8
Marc Andre Tanner <mat@brain-dump.org>
2009-10-10 15:39:50 +0200

    dialog-elementary: copy binary also to $STAGING_DIR
    Signed-off-by: Marc Andre Tanner <mat@brain-dump.org>

--- sources/sections/dialog-elementary.sh
+++ sources/sections/dialog-elementary.sh
@@ -3,10 +3,13 @@ setupfor dialog-elementary
 $CC $CFLAGS $CROSS_CFLAGS `pkg-config --cflags --libs elementary` \
 	-lz -ljpeg -lfreetype -lts dialog.c -o dialog || dienow 
 
+cp dialog $STAGING_DIR/usr/bin || dienow
+cp dialog $ROOT_DIR/usr/bin || dienow
+
 #$CC $CFLAGS $CROSS_CFLAGS `pkg-config --cflags --libs elementary --static` \
 #	-lz -ljpeg -lfreetype -lts -static dialog.c -o dialog-static || dienow 
 
-cp dialog $ROOT_DIR/usr/bin || dienow 
+#cp dialog-static $STAGING_DIR/usr/bin || dienow
 #cp dialog-static $ROOT_DIR/usr/bin || dienow 
 
 cleanup dialog-elementary 
