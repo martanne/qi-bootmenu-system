fe44f6e241c495abb156df3a0f13dd61c05eb0d9
Marc Andre Tanner <mat@brain-dump.org>
2009-11-10 00:26:07 +0100

    busybox: clean up build instructions
    Signed-off-by: Marc Andre Tanner <mat@brain-dump.org>

--- sources/sections/busybox.sh
+++ sources/sections/busybox.sh
@@ -1,6 +1,5 @@
-# Build and install busybox
-
 setupfor busybox
+
 make allnoconfig KCONFIG_ALLCONFIG="$CONFIG_DIR/miniconfig-busybox" &&
 cp .config "$CONFIG_DIR/config-busybox" &&
 LDFLAGS="$CROSS_LDFLAGS $LDFLAGS" make -j $CPUS CROSS_COMPILE="$CROSS" $VERBOSITY &&
@@ -11,9 +10,7 @@ cp busybox "$ROOT_DIR/usr/bin"
 
 for i in $(sed 's@.*/@@' busybox.links)
 do
-  # Allowed to fail.
   ln -s busybox "$ROOT_DIR/usr/bin/$i" 2>/dev/null
 done
-cd ..
 
 cleanup busybox
