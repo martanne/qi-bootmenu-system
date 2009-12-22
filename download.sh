501e9b7e5f43749893427d2746d145c39f8dd864
Marc Andre Tanner <mat@brain-dump.org>
2009-12-22 12:31:47 +0100

    qi-bootmenu: update to latest version, ignore NAND partition
    Mounting jffs2 just takes way too long:
    
    root@om-gta02 / $ time mount -t jffs2 /dev/mtdblock6 /mnt/mtdblock6/
    real    0m 6.46s
    user    0m 0.00s
    sys     0m 6.32s
    
    Signed-off-by: Marc Andre Tanner <mat@brain-dump.org>

--- download.sh
+++ download.sh
@@ -102,7 +102,7 @@ URL=http://ftp.gnu.org/gnu/gdb/gdb-7.0.tar.bz2 \
 SHA1=8aed621b7ddb8c82b6ff213b56c028787db90582 \
 download || dienow
 
-URL=http://repo.or.cz/w/qi-bootmenu.git/snapshot/ae9e0e4ffed61617f53fc625575464cb7e3103f1.tar.gz \
+URL=http://repo.or.cz/w/qi-bootmenu.git/snapshot/b99fe9b770b2b7a94a140e60f1ec9a37e91b2543.tar.gz \
 SHA1= \
 RENAME="s/(.*)/qi-bootmenu-\1/" \
 download || dienow
