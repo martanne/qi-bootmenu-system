28672863835e735db2aea86762e62df2b5ec43e1
Marc Andre Tanner <mat@brain-dump.org>
2010-10-04 22:17:14 +0200

    qi-bootmenu: update to latest git HEAD
    Signed-off-by: Marc Andre Tanner <mat@brain-dump.org>

--- download.sh
+++ download.sh
@@ -85,8 +85,9 @@ URL=http://ftp.gnu.org/gnu/gdb/gdb-7.0.tar.bz2 \
 SHA1=8aed621b7ddb8c82b6ff213b56c028787db90582 \
 download || dienow
 
-#SHA1=e57005075db77e21aab0ba509f4f0fe4a48283d6 \
-URL=http://www.brain-dump.org/projects/qi-bootmenu/qi-bootmenu-0.1.tar.gz \
+URL=http://repo.or.cz/w/qi-bootmenu.git/snapshot/5cb4e35904ae0084ac8bddaeab285ab744f3b0bf.tar.gz \
+SHA1= \
+RENAME="s/(.*)/qi-bootmenu-\1/" \
 download || dienow
 
 URL='http://git.openmoko.org/?p=qi.git;a=snapshot;h=c38b062a609f1442e6a9e13005cfbdfd59a5ac0d;sf=tgz' \
